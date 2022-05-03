79d6f0a0ed1857f355f4a86b755f9900ab7f47b2
Seven203190709 <103395223+Seven203190709@users.noreply.github.com>
2022-05-03 21:17:50 +0800

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  var x;
+
+  x = document.getElementById("myText").value;
+
+  if (x=="students") {
+  window.location.replace("https://www.w3schools.com/")
+  } else if (x=="parents"){
+  window. location.replace("https://www.w3schools.com/")
+  
+  }
+  else if (x!="students"){
+  alert("Please enter 'students' or 'parents'!")
+  }
+  else if (x!="parents"){
+  alert("Please enter 'students' or 'parents'!")
+  }
+  }
